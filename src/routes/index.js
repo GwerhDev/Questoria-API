533c333ef068e5f716d69abe1942eddcbc6641c1
Lalo Freak <lalofreak.dev@gmail.com>
2023-10-11 15:54:32 -0300

    adding signup system

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const auth = require('../controllers/auth');
 const loginGoogle = require('../controllers/login-google');
 const signupGoogle = require('../controllers/signup-google');
 
+router.use("/auth", auth);
 router.use("/login-google", loginGoogle);
 router.use("/signup-google", signupGoogle);
 
