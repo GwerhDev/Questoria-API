f1925ac8da4f7e460aeac7dbbf2b8c9514a520a1
Lalo Freak <lalofreak.dev@gmail.com>
2023-10-11 17:20:43 -0300

    adding signup system

---
+++ src/controllers/account.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const userSchema = require("../models/User");
+const { message } = require("../messages");
+const { decodeToken } = require("../integrations/jwt");
+const bcrypt = require("bcrypt");
+
+router.get("/my-data", async (req, res) => {
+  try {
+    const userToken = req.headers.authorization;
+    console.log(userToken)
+    const decodedToken = await decodeToken(userToken);
+    const user = await userSchema.findOne({_id: decodedToken.data.id});
+
+    if(!user) return res.status(404).send({ logged: false, message: message.user.notfound });
+
+    const userData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      isVerified: user.isVerified,
+      role: user.role,
+      profilePic: user.profilePic || user.googlePic
+    }
+
+
+    return res.status(200).send({ logged: true, userData });
+  } catch (error) {
+    return res.status(500).send({ error: message.user.error });
+  }
+});
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const auth = require('../controllers/auth');
+const account = require('../controllers/account');
 const loginGoogle = require('../controllers/login-google');
 const signupGoogle = require('../controllers/signup-google');
 
 router.use("/auth", auth);
+router.use("/account", account);
 router.use("/login-google", loginGoogle);
 router.use("/signup-google", signupGoogle);
 
